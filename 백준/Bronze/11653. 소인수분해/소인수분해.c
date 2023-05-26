f2872322d33461dc4ef1344139b63e4d49d9886b
Yun-Jinwoo <127362044+Yun-Jinwoo@users.noreply.github.com>
2023-05-26 18:49:26 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+
+int main()
+{
+	int N;
+
+	scanf("%d", &N);
+
+	for (int i = 2; i <= N; i++)
+	{
+		if (N % i == 0)
+		{
+			printf("%d\n", i);
+			N = N / i;
+			i = 1;
+		}
+	}
+}
