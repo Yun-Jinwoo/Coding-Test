3593fd244c1c7ba73f8bacac42f70717237efe25
Yun-Jinwoo <127362044+Yun-Jinwoo@users.noreply.github.com>
2024-01-06 18:55:25 +0900

    [Bronze II] Title: 수 정렬하기, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] > arr[j]) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << endl;
+	}
+	delete[] arr;
+	return 0;
+}
