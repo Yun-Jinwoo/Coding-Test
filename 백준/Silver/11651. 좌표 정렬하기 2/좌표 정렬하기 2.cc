434a7102818278abb7699ebb1e407dfd73e74833
Yun-Jinwoo <127362044+Yun-Jinwoo@users.noreply.github.com>
2024-01-10 16:29:36 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 144 ms, Memory: 5108 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge_sort(vector<pair<int,int>>& A, int first, int last) {
+	if (first >= last) return;
+	int middle = (first + last) / 2;
+	merge_sort(A, first, middle);
+	merge_sort(A, middle+1, last);
+	vector<pair<int,int>> B;
+	int i = first;
+	int j = middle + 1;
+	while (i <= middle && j <= last) {
+		if (A[i] <= A[j]) {
+			B.push_back(A[i]);
+			i++;
+		}
+		else {
+			B.push_back(A[j]);
+			j++;
+		}
+	}
+	for (; i <= middle; i++) {
+		B.push_back(A[i]);
+	}
+	for (; j <= last; j++) {
+		B.push_back(A[j]);
+	}
+	for (int k = first; k <= last; k++) {
+		A[k] = B[k - first];
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector<pair<int, int>> vec;
+	int x, y;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		vec.push_back({ y,x });
+	}
+	merge_sort(vec, 0, n - 1);
+	
+	for (auto a : vec) {
+		cout << a.second << " " << a.first << "\n";
+	}
+	return 0;
+}
