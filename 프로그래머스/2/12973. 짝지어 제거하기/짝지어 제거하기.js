ff68721a729bcd39dd6fa94739fd4d89d38b5d94
윤진우 <yjw3480@naver.com>
2025-07-10 00:27:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 90.64 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,12 +1,16 @@
 function solution(s) {
-    let top = -1;
-    const stack = new Array(s.length);
-    for (let i = 0; i < s.length; i++) {
-        if (top >= 0 && stack[top] === s[i]) {
-            top--; // pop
-        } else {
-            stack[++top] = s[i]; // push
+    let stack = [];
+    for(let ch of s){
+        if(stack.length !== 0){
+            if(ch === stack.at(-1)){
+                stack.pop();
+            } else{
+                stack.push(ch);
+            }
+        } else{
+            stack.push(ch)
         }
     }
-    return top === -1 ? 1 : 0;
+    
+    return stack.length === 0 ? 1 : 0
 }
