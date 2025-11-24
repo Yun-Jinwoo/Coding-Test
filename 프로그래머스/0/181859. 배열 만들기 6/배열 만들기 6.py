e41dc9ada455d378beec890ed04ab80a15b3ac73
윤진우 <yjw3480@naver.com>
2025-11-24 20:37:00 +0900

    [level 0] Title: 배열 만들기 6, Time: 279.21 ms, Memory: 17.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,15 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr):
+        if len(stk) == 0:
+            stk.append(arr[i])
+        elif stk[-1] == arr[i]:
+            stk.pop()
+        else:
+            stk.append(arr[i])
+        i += 1
+        
+    if len(stk) == 0:
+        return [-1]
+    return stk
