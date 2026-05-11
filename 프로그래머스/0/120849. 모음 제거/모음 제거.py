5e9eecf88f13d20c659a27fcd2d9c2aaca2d7ef9
윤진우 <yjw3480@naver.com>
2026-05-11 21:57:58 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    ans = ''
+    
+    for i in range(len(my_string)):
+        if my_string[i] not in ['a','e','i','o','u']:
+            ans += my_string[i]
+    
+    return ans
+
