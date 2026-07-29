16088cd049d1423ce8f928fc60a602f291b4c173
윤진우 <yjw3480@naver.com>
2026-07-29 21:42:17 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.34 ms
+메모리: 11.5 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 22일 23:28:51
+2026년 07월 29일 21:42:10
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,17 @@
+def solution(n, lost, reserve):
+    lost.sort()
+    reserve.sort()
+    reserve_copy = reserve[:]
+    
+    for i in reserve_copy:
+        if i in lost:
+            lost.remove(i)
+            reserve.remove(i)
+    
+    for i in reserve:
+        if i-1 in lost:
+            lost.remove(i-1)
+        elif i+1 in lost:
+            lost.remove(i+1)
+
+    return n - len(lost)
