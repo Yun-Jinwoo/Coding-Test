fd77838d90a8611892dc2c276c7ce38016ac883c
윤진우 <yjw3480@naver.com>
2026-08-05 19:03:08 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.07 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,17 @@
+def solution(data, ext, val_ext, sort_by):
+    idx = {
+        "code": 0,
+        "date": 1,
+        "maximum": 2,
+        "remain": 3
+    }
+
+    answer = []
+
+    for d in data:
+        if d[idx[ext]] < val_ext:
+            answer.append(d)
+
+    answer.sort(key=lambda x: x[idx[sort_by]])
+
+    return answer
